c3b68aac726c92430409581b2a52cd31676baa5f
unknown <ngoctrangnguyen1701@gmail.com>
2022-02-24 09:28:14 +0700

    cai tien function attack

--- FileFirstProject/script/main.js
+++ FileFirstProject/script/main.js
@@ -39,19 +39,21 @@ new Vue({
       if(this.isYourTurn === false && !this.winner){
         console.log('monsterAttack');
         //nếu không phải lượt của You và chưa có người thắng
-        const minDamage = 5
-        const maxDamage = 40
-        const damageYou = Math.floor(Math.random() * (maxDamage - minDamage + 1) ) + minDamage
-        this.healthbarYou -= damageYou
-        this.isYourTurn = true
+        // const minDamage = 5
+        // const maxDamage = 40
+        // const damageYou = Math.floor(Math.random() * (maxDamage - minDamage + 1) ) + minDamage
+        // this.healthbarYou -= damageYou
+        this.healthbarYou -= this.setDamage(5, 40)
+        // this.isYourTurn = true
+        this.setWinner()
 
-        if(this.healthbarYou < 0){
-          this.winner = 'Monster' //--> nếu máu của You dưới 0, Monster sẽ thắng, và ngược lại
-          this.healthbarProgressYou = 0
-        }
-        else{
-          this.healthbarProgressYou = this.healthbarYou
-        }
+        // if(this.healthbarYou <= 0 && this.isStartGame){
+        //   this.winner = 'Monster' //--> nếu máu của You dưới 0, Monster sẽ thắng, và ngược lại
+        //   this.healthbarProgressYou = 0
+        // }
+        // else{
+        //   this.healthbarProgressYou = this.healthbarYou
+        // }
       }
     },
     // setWinner: function(){
@@ -83,22 +85,49 @@ new Vue({
     attack: function(){
       if(!this.winner){ //nếu chưa có người thắng
         console.log('attack');
-        const minDamage = 5
-        const maxDamage = 40
-        const damageMonster = Math.floor(Math.random() * (maxDamage - minDamage + 1) ) + minDamage
-        this.healthbarMonster -= damageMonster
-        this.isYourTurn = false
+        // const minDamage = 5
+        // const maxDamage = 40
+        // const damageMonster = Math.floor(Math.random() * (maxDamage - minDamage + 1) ) + minDamage
+        // this.healthbarMonster -= damageMonster
+        this.healthbarMonster -= this.setDamage(5, 40)
+        // this.isYourTurn = false
+        this.setWinner()
   
         // const damageMonster = Math.floor(Math.random() * (maxDamage - minDamage + 1) ) + minDamage
         // this.healthbarMonster -= damageMonster
 
-        if(this.healthbarMonster < 0){
+        // if(this.healthbarMonster <= 0 && this.isStartGame){
+        //   this.winner = 'You'
+        //   this.healthbarProgressMonster = 0
+        // }
+        // else{
+        //   this.healthbarProgressMonster = this.healthbarMonster
+        // }
+      }
+    },
+    setDamage: function(minDamage, maxDamage){
+      return Math.floor(Math.random() * (maxDamage - minDamage + 1) ) + minDamage
+    },
+    setWinner: function(){
+      if(this.isYourTurn){
+        if(this.healthbarMonster <= 0 && this.isStartGame){
           this.winner = 'You'
           this.healthbarProgressMonster = 0
         }
         else{
           this.healthbarProgressMonster = this.healthbarMonster
+          this.isYourTurn = false
         }
+      }
+      else{
+        if(this.healthbarYou <= 0 && this.isStartGame){
+          this.winner = 'Monster' //--> nếu máu của You dưới 0, Monster sẽ thắng, và ngược lại
+          this.healthbarProgressYou = 0
+        }
+        else{
+          this.healthbarProgressYou = this.healthbarYou
+          this.isYourTurn = true
+        }      
       }
     }
   }
