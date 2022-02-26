71da047004c0bc900346f11497f9f837a47ac204
ngoctrangnguyen1701 <89184893+ngoctrangnguyen1701@users.noreply.github.com>
2022-02-26 23:47:35 +0700

    add audio

--- FileFirstProject/scripts/main.js
+++ FileFirstProject/scripts/main.js
@@ -1,3 +1,14 @@
+const audio = {
+  startGame: document.getElementById('start-game-audio'),
+  alarm: document.getElementById('alarm-audio'),
+  gameOver: document.getElementById('game-over-audio'),
+  heal: document.getElementById('heal-audio'),
+  monsterLaugh: document.getElementById('monster-laugh-audio'),
+  punch: document.getElementById('punch-audio'),
+  victory: document.getElementById('victory-audio'),
+  victory2: document.getElementById('victory2-audio'),
+}
+
 const initialGame = {
   healthbarYou: 0,
   healthbarMonster: 0,
@@ -12,6 +23,11 @@ const initialGame = {
   isDamageMonster: false,
   isHealYou: false,
   timeOfHeal: 3, //số lần hồi máu trong quá 3 lần
+
+  isMute: false,
+  isAlarm: false,
+  youWin: 0,
+  monsterWin: 0,
 }
 
 new Vue({
@@ -26,7 +42,6 @@ new Vue({
         setTimeout(()=>{
           console.log('monsterAttack');
           //nếu không phải lượt của You và chưa có người thắng
-          // this.healthbarYou -= this.randomNumber(5, 40)
           this.damageIndex = this.randomNumber(5, 40)
           this.healthbarYou -= this.damageIndex
 
@@ -58,11 +73,14 @@ new Vue({
       this.isDamageMonster = false
       this.isHealYou = false
       this.timeOfHeal = 3
+
+      this.isAlarm = false
+      //CONTROL AUDIO
+      if(!this.isMute) audio.startGame.play()
     },
     attack: function(){
       if(!this.winner){ //nếu chưa có người thắng
         console.log('you attack')
-        // this.healthbarMonster -= this.randomNumber(10, 20)
         this.damageIndex = this.randomNumber(10, 20)
         this.healthbarMonster -= this.damageIndex
 
@@ -76,7 +94,7 @@ new Vue({
       if(!this.winner){
         if(this.healthbarYou <= 60 && !this.winner && this.timeOfHeal !== 0){
           console.log('you heal')
-          this.healIndex = this.randomNumber(20, 40)
+          this.healIndex = this.randomNumber(20, 50)
           this.healthbarYou += this.healIndex
           this.healthbarProgressYou = this.healthbarYou
           this.isYourTurn = false
@@ -86,6 +104,12 @@ new Vue({
           this.isDamageYou = false
           this.isHealYou = true
           this.timeOfHeal -= 1
+
+          this.isAlarm = false 
+          //chỉnh lại isAlarm = false khi hồi máu, 
+          //để khi máu You bị Monster attack, tuột xuống dưới 20 sẽ báo động 1 lần nữa
+          //CONTROL AUDIO
+          if(!this.isMute) audio.heal.play()
         }
         else if(this.timeOfHeal === 0){
           alert('You can not heal anymore!')
@@ -99,6 +123,13 @@ new Vue({
       //nếu bấm give up, Monster sẽ win
       this.winner = 'Monster'
       this.isStartGame = false
+      this.monsterWin += 1
+
+      //CONTROL AUDIO
+      if(!this.isMute) {
+        audio.gameOver.play()
+        audio.monsterLaugh.play()
+      }
     },
     randomNumber: function(min, max){
       return Math.floor(Math.random() * (max - min + 1) ) + min
@@ -109,10 +140,20 @@ new Vue({
           this.winner = 'You'
           this.healthbarProgressMonster = 0
           this.isStartGame = false
+          this.youWin += 1
+
+          //CONTROL AUDIO
+          if(!this.isMute) {
+            audio.victory.play()
+            audio.victory2.play()
+          }
         }
         else{
           this.healthbarProgressMonster = this.healthbarMonster
           this.isYourTurn = false
+
+          //CONTROL AUDIO
+          if(!this.isMute) audio.punch.play()
         }
       }
       else{
@@ -120,10 +161,26 @@ new Vue({
           this.winner = 'Monster' //--> nếu máu của You dưới 0, Monster sẽ thắng, và ngược lại
           this.healthbarProgressYou = 0
           this.isStartGame = false
+          this.monsterWin += 1
+
+          //CONTROL AUDIO
+          if(!this.isMute) {
+            audio.gameOver.play()
+            audio.monsterLaugh.play()
+          }
         }
         else{
           this.healthbarProgressYou = this.healthbarYou
           this.isYourTurn = true
+
+          if(!this.isMute) audio.punch.play()
+
+          if(this.healthbarYou <= 30){
+            //báo động khi máu của You dưới 30
+            this.isAlarm = true
+            //CONTROL AUDIO
+            if(!this.isMute) audio.alarm.play()
+          }
         }      
       }
     },
